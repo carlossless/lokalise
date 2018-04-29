84513d2003d3555738b6b1addb8d20dfbbc9f5a0
Dave Williams <davnicwil@gmail.com>
2018-04-29 14:28:33 +0100

    Add keysFile config validations, and tests

--- src/config.test.js
+++ src/config.test.js
@@ -8,20 +8,67 @@ describe('config', () => {
       delete process.env.LOKALISE_TOKEN
       delete process.env.LOKALISE_PROJECT
       delete process.env.LOKALISE_OUTPUT
+      delete process.env.LOKALISE_KEYS_FILE
     })
 
     it('loads all environment var config values', async () => {
-      expect.assertions(1)
+      expect.assertions(4)
+
       process.env.LOKALISE_TOKEN = 'test_token'
       process.env.LOKALISE_PROJECT = 'test_project'
       process.env.LOKALISE_OUTPUT = 'test_output'
 
-      const conf = await config.build(null, { })
+      // with no LOKALISE_KEYS_FILE set
+      const conf1 = await config.build(null, { })
 
-      expect(conf).toEqual({
+      expect(conf1).toEqual({
         token: 'test_token',
         project: 'test_project',
         output: 'test_output'
+      })
+
+      // with LOKALISE_KEYS_FILE set true, should be all default settings
+      process.env.LOKALISE_KEYS_FILE = 'true'
+
+      const conf2 = await config.build(null, { })
+
+      expect(conf2).toEqual({
+        token: 'test_token',
+        project: 'test_project',
+        output: 'test_output',
+        keysFile: {
+          name: 'keys.json',
+          type: 'json',
+          flow: false
+        }
+      })
+
+      // with LOKALISE_KEYS_FILE set false
+      process.env.LOKALISE_KEYS_FILE = 'false'
+
+      const conf3 = await config.build(null, { })
+
+      expect(conf3).toEqual({
+        token: 'test_token',
+        project: 'test_project',
+        output: 'test_output'
+      })
+
+      // with LOKALISE_KEYS_FILE config specifified as JSON
+      process.env.LOKALISE_KEYS_FILE = '{ "name": "custom.js", "type": "es6", "flow": true }'
+
+      const conf4 = await config.build(null, { })
+
+      expect(conf4).toEqual({
+        token: 'test_token',
+        project: 'test_project',
+        output: 'test_output',
+        // should match passed JSON
+        keysFile: {
+          name: 'custom.js',
+          type: 'es6',
+          flow: true
+        }
       })
     })
 
