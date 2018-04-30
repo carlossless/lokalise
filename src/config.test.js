d22fa570959ad749631c89b9bf20a8d202ef08e7
Dave Williams <davnicwil@gmail.com>
2018-04-30 19:36:57 +0100

    Separate test cases for different types of env var configs

--- src/config.test.js
+++ src/config.test.js
@@ -11,64 +11,90 @@ describe('config', () => {
       delete process.env.LOKALISE_KEYS_FILE
     })
 
-    it('loads all environment var config values', async () => {
-      expect.assertions(4)
+    describe('environment var config values', () => {
+      it('loads environment var config values with no LOKALISE_KEYS_FILE set', async () => {
+        expect.assertions(1)
 
-      process.env.LOKALISE_TOKEN = 'test_token'
-      process.env.LOKALISE_PROJECT = 'test_project'
-      process.env.LOKALISE_OUTPUT = 'test_output'
+        process.env.LOKALISE_TOKEN = 'test_token'
+        process.env.LOKALISE_PROJECT = 'test_project'
+        process.env.LOKALISE_OUTPUT = 'test_output'
 
-      // with no LOKALISE_KEYS_FILE set
-      const conf1 = await config.build(null, { })
+        const conf = await config.build(null, { })
 
-      expect(conf1).toEqual({
-        token: 'test_token',
-        project: 'test_project',
-        output: 'test_output'
+        expect(conf).toEqual({
+          token: 'test_token',
+          project: 'test_project',
+          output: 'test_output'
+        })
       })
 
-      // with LOKALISE_KEYS_FILE set true, should be all default settings
-      process.env.LOKALISE_KEYS_FILE = 'true'
+      it('loads environment var config values with LOKALISE_KEYS_FILE=true', async () => {
+        expect.assertions(1)
 
-      const conf2 = await config.build(null, { })
+        process.env.LOKALISE_TOKEN = 'test_token'
+        process.env.LOKALISE_PROJECT = 'test_project'
+        process.env.LOKALISE_OUTPUT = 'test_output'
 
-      expect(conf2).toEqual({
-        token: 'test_token',
-        project: 'test_project',
-        output: 'test_output',
-        keysFile: {
+        process.env.LOKALISE_KEYS_FILE = 'true'
+
+        const conf = await config.build(null, { })
+
+        const expectedDefaultKeysFileValues = {
           name: 'keys.json',
           type: 'json',
           flow: false
         }
+
+        expect(conf).toEqual({
+          token: 'test_token',
+          project: 'test_project',
+          output: 'test_output',
+          keysFile: expectedDefaultKeysFileValues
+        })
       })
 
-      // with LOKALISE_KEYS_FILE set false
-      process.env.LOKALISE_KEYS_FILE = 'false'
+      it('loads environment var config values with LOKALISE_KEYS_FILE=false', async () => {
+        expect.assertions(1)
 
-      const conf3 = await config.build(null, { })
+        process.env.LOKALISE_TOKEN = 'test_token'
+        process.env.LOKALISE_PROJECT = 'test_project'
+        process.env.LOKALISE_OUTPUT = 'test_output'
 
-      expect(conf3).toEqual({
-        token: 'test_token',
-        project: 'test_project',
-        output: 'test_output'
+        process.env.LOKALISE_KEYS_FILE = 'false'
+
+        const conf = await config.build(null, { })
+
+        expect(conf).toEqual({
+          token: 'test_token',
+          project: 'test_project',
+          output: 'test_output'
+        })
       })
 
-      // with LOKALISE_KEYS_FILE config specifified as JSON
-      process.env.LOKALISE_KEYS_FILE = '{ "name": "custom.js", "type": "es6", "flow": true }'
+      it('loads environment var config values with custom LOKALISE_KEYS_FILE values', async () => {
+        expect.assertions(1)
 
-      const conf4 = await config.build(null, { })
+        process.env.LOKALISE_TOKEN = 'test_token'
+        process.env.LOKALISE_PROJECT = 'test_project'
+        process.env.LOKALISE_OUTPUT = 'test_output'
 
-      expect(conf4).toEqual({
-        token: 'test_token',
-        project: 'test_project',
-        output: 'test_output',
-        // should match passed JSON
-        keysFile: {
+        process.env.LOKALISE_KEYS_FILE = '{ "name": "custom.js", "type": "es6", "flow": true }'
+
+        const conf = await config.build(null, { })
+
+        // matches JSON value passed as LOKALISE_KEYS_FILE
+        const expectedKeysFileValues = {
           name: 'custom.js',
           type: 'es6',
           flow: true
         }
+
+        expect(conf).toEqual({
+          token: 'test_token',
+          project: 'test_project',
+          output: 'test_output',
+          keysFile: expectedKeysFileValues
+        })
       })
     })
 
